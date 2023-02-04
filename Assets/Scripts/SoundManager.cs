58fb026eb291b4039283ea60f00b1b78700c533d
kdeppmeier <kdeppmeier@yahoo.com>
2023-02-04 15:43:31 -0800

    Updated SoundManager, added scene switching

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Contains the dictionary of sound effects and has methods for playing/stopping sounds
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager SoundInstance = null;
 
-    private AudioSource audioSourceObject;
+    private AudioSource[] audioSourceObjects;
 
     private Dictionary<string, AudioClip> soundDictionary = new Dictionary<string, AudioClip>();
     
@@ -16,28 +17,51 @@ public class SoundManager : MonoBehaviour
         if (SoundInstance == null)
             SoundInstance = this;
         else if (SoundInstance != this)
+        {
             Destroy(gameObject);
+        }
 
         DontDestroyOnLoad(gameObject);
 
-        audioSourceObject = gameObject.GetComponent<AudioSource>();
-        if (audioSourceObject == null)
+        audioSourceObjects = gameObject.GetComponents<AudioSource>();
+        if (audioSourceObjects == null || audioSourceObjects.Length != 2)
             Debug.Log("AudioSource not found");
+        else
+            Debug.Log(audioSourceObjects.Length + " AudioSource objects found");
 
         LoadSFX();
+        PlayMusic("htl");
     }
 
     //Maps sound clips to key strings in a dictionary
     private void LoadSFX()
     {
         soundDictionary.Add("click", Resources.Load("Sounds/Click") as AudioClip);
-        Debug.Log(soundDictionary.Count);
+        soundDictionary.Add("htl", Resources.Load("Sounds/htl") as AudioClip);
+        //Debug.Log("Loaded " + soundDictionary.Count + " clips");
+    }
+
+    //Plays music using its name.  Will loop
+    public void PlayMusic(string clipName)
+    {
+        audioSourceObjects[0].clip = soundDictionary.GetValueOrDefault(clipName);
+        audioSourceObjects[0].loop = true;
+        //audioSourceObjects[0].Play();
+        audioSourceObjects[0].Play();
+    }
+
+    //Stops the music
+    public void StopMusic()
+    {
+        audioSourceObjects[0].Stop();
     }
 
     //Plays a clip using its name
     public void PlaySFX(string clipName)
     {
-        audioSourceObject.clip = soundDictionary.GetValueOrDefault(clipName);
-        audioSourceObject.Play();
+        Debug.Log(soundDictionary.Count + " " + audioSourceObjects.Length);
+        audioSourceObjects[1].clip = soundDictionary.GetValueOrDefault(clipName);
+        audioSourceObjects[1].loop = false;
+        audioSourceObjects[1].Play();
     }
 }
